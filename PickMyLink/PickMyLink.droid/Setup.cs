6d9b8bf7e25b1776715aa441b56a42536e0819cc
Noam Ofek <noamo@rumble.me>
2014-07-24 16:21:35 +0300

    First Check in

---
+++ PickMyLink/PickMyLink.droid/Setup.cs
@@ -0,0 +1,24 @@
+using Android.Content;
+using Cirrious.CrossCore.Platform;
+using Cirrious.MvvmCross.Droid.Platform;
+using Cirrious.MvvmCross.ViewModels;
+
+namespace PickMyLink.droid
+{
+    public class Setup : MvxAndroidSetup
+    {
+        public Setup(Context applicationContext) : base(applicationContext)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+			return new PickMyLink.App();
+        }
+		
+        protected override IMvxTrace CreateDebugTrace()
+        {
+            return new DebugTrace();
+        }
+    }
+}
---
+++ PickMyLink/PickMyLink.ios/RootViewController.designer.cs
@@ -0,0 +1,14 @@
+﻿using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
+
+namespace PickMyLink.ios
+{
+	[Register ("RootViewController")]
+	partial class RootViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
+
